57813a287e41c47269f877376b1820977910f94e
kavinbalagen <kavin_arasu.balasubramanian@genesys.com>
2024-06-13 19:55:03 -0400

    wrote the commands.sh script

--- jenkins/commands.sh
+++ jenkins/commands.sh
@@ -2,4 +2,15 @@ GIT_ACCESS_TOKEN=$1
 
 
 echo "GIT_ACCESS_TOKEN = $GIT_ACCESS_TOKEN"
-echo "RAN SCHELL SCRIPT"
+
+echo "DOWNLOADING DOCS"
+#curl https://github.com/kavinbala96/weatherapp/raw/main/docs.zip -L -O -J
+
+
+echo "EXTRACTING DOCS"
+pwd
+mkdir docs
+unzip docs.zip -d docs
+rm -r -f public/docs
+mv -f docs public/
+
