03c2a133c71c8e015520e765fb0e735384ecd824
kavinbalagen <kavin_arasu.balasubramanian@genesys.com>
2024-06-20 10:11:19 -0400

    deleted docs

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -11,7 +11,7 @@ const MarkdownViewer: React.FC<MarkdownViewerProps> = ({ file }) => {
 
   useEffect(() => {
     if (file) {
-      fetch(`/${APP_NAME}/docs/${file}.json`)
+      fetch(`/${APP_NAME}/docs/${file}`)
         .then(response => response.text())
         .then(text => setContent(text))
         .catch(error => console.error('Error fetching markdown file:', error));
