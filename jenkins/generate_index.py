f93563451885b7473f34f1c064ce1cd9fbb4a2fa
kavinbalagen <kavin_arasu.balasubramanian@genesys.com>
2024-06-20 13:20:01 -0400

    changed genfile

--- jenkins/generate_index.py
+++ jenkins/generate_index.py
@@ -4,19 +4,28 @@ import sys
 import fileinput
 
 APP_NAME_LIST=['package.json','package-lock.json','src/components/Constants.tsx']
+DOCS_DIR = 'public/docs'
+NAMES_DIR = 'public/names.json'
 
-def generate_index(path_to_directory):
+def process_files():
     file_names=[]
-    for file in os.listdir(path_to_directory):
-        file_path = os.path.join(path_to_directory,file)
-        if os.path.isfile(file_path):
-            file_names.append(file)
+    # removing files other than .md
+    for file in os.listdir(DOCS_DIR):
+        file_path = os.path.join(DOCS_DIR,file)
+        if os.path.isfile(file_path) and file.endswith('.md'):
+            file_names.append(file[:-3]+'.json')
+        else:
+            os.remove(file_path)
+    # renaming to .json, thank github :(
+    for file in os.listdir(DOCS_DIR):
+        file_path = os.path.join(DOCS_DIR,file)
+        os.replace(file_path,file_path[:-3]+'.json')
     file_names.sort()
     return {"names":file_names}
 
 def saving_index_file():
-    with open('public/names.json','w+') as file:
-        file_names=generate_index('public/docs')
+    with open(NAMES_DIR,'w+') as file:
+        file_names=process_files()
         json.dump(file_names,file,indent=4)
 
 def renaming_app():
@@ -25,8 +34,10 @@ def renaming_app():
             for line in file:
                 print(line.replace("{{APP_NAME}}", sys.argv[1]), end='')
 
+
 if __name__ == '__main__':
     saving_index_file()
-    renaming_app()
+    #renaming_app()
     ### TO BE COMMENTED OUT ###
+
     print("PYSCRIPT "+sys.argv[1])
